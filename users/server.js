d1af5848a9fca58345a4fdb8d3c95847bac43e6f
Sourav <people.outofbox@gmail.com>
2022-12-11 13:18:31 +0530

    load balancer

---
+++ users/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+var http = require('http');
+var server = http.createServer(app);
+
+
+let allUsers = [
+    { userId: 1, name: 'User 1' },
+    { userId: 2, name: 'User 2' },
+    { userId: 3, name: 'User 3' }
+];
+
+app.get('/', (req, res) => {
+    res.send({ status: 'success', port: server.address().port, data: [...allUsers] });
+});
+
+
+server.listen(4002., () => {
+    console.log('USERS microservice started at port 4002')
+})
